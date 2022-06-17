c3c2c71a4a8a5d60e03d3526ef1d8fe236e3b2ba
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-06-17 10:30:43 -0300

    Exercícios de Algoritmos.

--- 013 - Conta banco com switch/013 - Conta banco com switch/Program.cs
+++ 013 - Conta banco com switch/013 - Conta banco com switch/Program.cs
@@ -8,71 +8,82 @@ namespace _013___Conta_banco_com_switch
         {
             int Saldo = 500, Saque, Deposito, Transferencia = 0;
             float Numero;
+            Boolean verdade = true;
 
-            Console.WriteLine("SEU SALDO ATUAL É DE 500,00");
-            Console.WriteLine("DIGITE 1 PARA SAQUE");
-            Console.WriteLine("DIGITE 2 PARA DEPOSITO");
-            Console.WriteLine("DIGITE 3 PARA TRANSFERENCIA");
-            Console.WriteLine("DIGITE 4 PARA EXTRATO");
-            Console.WriteLine("DIGITE 9 PARA SAIR");
-            Numero = int.Parse(Console.ReadLine());
-
-            switch (Numero)
+            while (verdade)
             {
 
-                case 1:
-                    Console.WriteLine("DIGITE O VALOR PARA SAQUE");
-                    Saque = int.Parse(Console.ReadLine());
-                    if (Saque > 500)
-                    {
-                        Console.WriteLine("SALDO INDISPONIVEL");
-                    }
-                    else
-                    {
-                        Saldo = Saldo - Saque;
-                        Console.WriteLine("SEU SALDO ATUAL E DE :" + Saldo);
-                    }
-                    break;
+                Console.WriteLine("SEU SALDO ATUAL É DE 500,00");
+                Console.WriteLine("DIGITE 1 PARA SAQUE");
+                Console.WriteLine("DIGITE 2 PARA DEPOSITO");
+                Console.WriteLine("DIGITE 3 PARA TRANSFERENCIA");
+                Console.WriteLine("DIGITE 4 PARA EXTRATO");
+                Console.WriteLine("DIGITE 9 PARA SAIR");
+                Numero = int.Parse(Console.ReadLine());
 
-                case 2:
-                    {
-                        Console.WriteLine("DIGITE O VALOR QUE DESEJA DEPOSITAR");
-                        Deposito = int.Parse(Console.ReadLine());
+                switch (Numero)
+                {
 
-                        if (Deposito > 0)
-                        {
-                            Saldo = Saldo + Deposito;
-                            Console.WriteLine("SEU SALDO ATUAL E DE :" + Saldo);
-                        }
-                        else
-                        {
-                            Console.WriteLine("ESSE VALOR NAO E VALIDO");
-                        }
-                        break;
-                    }
-                case 3:
-                    {
-                        Console.WriteLine("DIGITE O VALOR QUE DESEJA TRANSFERIR");
-                        Transferencia = int.Parse(Console.ReadLine());
-
-                        if (Transferencia > Saldo)
+                    case 1:
+                        Console.WriteLine("DIGITE O VALOR PARA SAQUE");
+                        Saque = int.Parse(Console.ReadLine());
+                        if (Saque > 500)
                         {
                             Console.WriteLine("SALDO INDISPONIVEL");
                         }
                         else
                         {
-                            Transferencia = Saldo - Transferencia;
-                            Saldo = Transferencia;
+                            Saldo = Saldo - Saque;
                             Console.WriteLine("SEU SALDO ATUAL E DE :" + Saldo);
                         }
                         break;
-                    }
-                case 4:
+
+                    case 2:
+                        {
+                            Console.WriteLine("DIGITE O VALOR QUE DESEJA DEPOSITAR");
+                            Deposito = int.Parse(Console.ReadLine());
+
+                            if (Deposito > 0)
+                            {
+                                Saldo = Saldo + Deposito;
+                                Console.WriteLine("SEU SALDO ATUAL E DE :" + Saldo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("ESSE VALOR NAO E VALIDO");
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("DIGITE O VALOR QUE DESEJA TRANSFERIR");
+                            Transferencia = int.Parse(Console.ReadLine());
+
+                            if (Transferencia > Saldo)
+                            {
+                                Console.WriteLine("SALDO INDISPONIVEL");
+                            }
+                            else
+                            {
+                                Transferencia = Saldo - Transferencia;
+                                Saldo = Transferencia;
+                                Console.WriteLine("SEU SALDO ATUAL E DE :" + Saldo);
+                            }
+                            break;
+                        }
+                        // case 9:
+                        //  {
+                        //  Environment.Exit(0);
+                        // }
+                        // break;
+                }
+
+                if (Numero == 9)
                     {
-                        Console.WriteLine("TECLE QUALQUER TECLA PARA SAIR");
-                    }
-                    break;
+                    verdade = false;
+                }
             }
+
         }
     }
 }
