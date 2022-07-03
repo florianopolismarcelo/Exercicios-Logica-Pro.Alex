528bcc48ad881ffe617398bab0ca8b889b6e5042
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-07-03 14:02:34 -0300

    Exercícios de Algoritmos.

---
+++ 014 - Media do aluno com while/014 - Media do aluno com while/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _014___Media_do_aluno_com_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Faça um programa que leia conjuntos de dois valores,
+            o primeiro representando o nome do aluno e o segundo representando a sua idade.
+            (Pare o programa inserindo o valor 0 no campo nome) */
+
+            String nome;
+            int idade;
+
+            while (true)
+            {
+                Console.WriteLine("Infome o nome do aluno");
+                nome = Console.ReadLine();
+
+                if (nome == "0")
+                    break;
+            }
+
+            Console.WriteLine("Infome a idade do aluno");
+            idade = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ 015 - Nota com while/015 - Nota com while/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _015___Nota_com_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa que peça uma nota, entre zero e dez.
+            Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido. */
+
+
+            int nota;
+
+            Console.WriteLine("Informe a nota do aluno");
+            nota = int.Parse(Console.ReadLine());
+
+            while (nota < 0 | nota > 10)
+            {
+                Console.WriteLine("Nota Inválida! Digite novamente: ");
+                break;
+            }
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Você digitou a nota: " +nota);
+            Console.WriteLine("-----------------------");
+
+        }
+    }
+}
