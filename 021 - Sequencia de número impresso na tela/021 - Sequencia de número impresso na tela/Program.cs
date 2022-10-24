69daeaab0a923bfa69b8d278fea927e286784827
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-24 07:02:44 -0300

    Exercícios de Algoritmos.

---
+++ 021 - Sequencia de número impresso na tela/021 - Sequencia de número impresso na tela/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*********************************************************************************
+06 - Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro.
+*********************************************************************************/
+
+int numero = 0;
+
+for (int i = 1; i <= 20; i++)
+{
+    //Console.WriteLine(i);
+    Console.Write(i + "  ");
+}
+Console.WriteLine("\n----------------------------------------------------------------------");
