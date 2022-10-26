0c3a18758f5207dc761d733b6551f0914aa91387
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-26 06:20:13 -0300

    Exercícios de Algoritmos.

---
+++ 022 - Maior número entre 5/022 - Maior número entre 5/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*****************************************************************
+06 - Faça um programa que leia 5 números e informe o maior número.
+*****************************************************************/
+
+int i = 0, numero, maior = 0;
+
+do
+{
+    Console.WriteLine("Digite um número: ");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero > maior)
+        maior = numero;
+    i = i + 1;
+
+} while (i < 5);
+Console.WriteLine("Maior: " + maior);
