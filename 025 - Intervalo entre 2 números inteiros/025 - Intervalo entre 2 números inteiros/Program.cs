056eb7f88603f068c7c75174fff12b7f82e01458
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-26 06:33:51 -0300

    Exercícios de Algoritmos.

---
+++ 025 - Intervalo entre 2 números inteiros/025 - Intervalo entre 2 números inteiros/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero1, numero2;
+
+Console.WriteLine("Informe o primeiro número inteiro:");
+numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o segundo número inteiro:");
+numero2 = int.Parse(Console.ReadLine());
+
+for (int i = numero1; i <= numero2; i++)
+{
+    Console.WriteLine(i);
+}
+//Console.ReadKey();
