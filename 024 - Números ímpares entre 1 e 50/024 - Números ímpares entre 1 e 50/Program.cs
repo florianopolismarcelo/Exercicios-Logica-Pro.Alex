02c0aa02b59b4f7a149174ac7b6fee584d02d118
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-26 06:30:25 -0300

    Exercícios de Algoritmos.

---
+++ 024 - Números ímpares entre 1 e 50/024 - Números ímpares entre 1 e 50/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/********************************************************************************
+09 - Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+********************************************************************************/
+
+int x;
+
+for (int i = 1; i <= 50; i++)
+{
+    x = i;
+    if (x % 2 != 0)
+        Console.WriteLine(x);
+}
