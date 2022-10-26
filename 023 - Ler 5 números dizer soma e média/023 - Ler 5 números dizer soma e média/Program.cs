acb96866265dac8772866669f3642d18588551b6
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-26 06:23:39 -0300

    Exercícios de Algoritmos.

---
+++ 023 - Ler 5 números dizer soma e média/023 - Ler 5 números dizer soma e média/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 0, soma = 0, numero, maior = 0;
+
+do
+{
+    Console.Write("Número: ");
+    numero = int.Parse(Console.ReadLine());
+
+    soma = soma + numero;
+    i = i + 1;
+
+} while (i < 5);
+
+Console.WriteLine("");
+Console.WriteLine($"A soma de todos os números é: {soma} ");
+Console.WriteLine("A média é: " + (soma / 5));
