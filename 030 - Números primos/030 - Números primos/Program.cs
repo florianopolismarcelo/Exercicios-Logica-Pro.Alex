af26c88115ba2a7279b3c9c676407b33c560952c
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-27 06:38:59 -0300

    Exercícios de Algoritmos.

---
+++ 030 - Números primos/030 - Números primos/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Numeros_primos;
+using System;
+
+namespace Numeros_primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            for (int i = 2; i <= 100; i++)
+            {
+                if (ehPrimo(i))
+                    Console.WriteLine(i + " é primo.");
+            }
+        }
+        private static bool ehPrimo(int numero)
+
+        {
+            for (int j = 2; j < numero; j++)
+            {
+                if (numero % j == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
