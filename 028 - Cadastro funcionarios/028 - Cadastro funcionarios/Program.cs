35d9121de01c97c88a728846c70d83eb5d0b220b
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-27 06:27:17 -0300

    Exercícios de Algoritmos.

---
+++ 028 - Cadastro funcionarios/028 - Cadastro funcionarios/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int opcao;
+float addSalario = 0;
+string addNome = "";
+string addCargo = "";
+Boolean verdade = true;
+
+
+List<string> nome = new List<string>();
+List<string> cargo = new List<string>();
+List<float> salario = new List<float>();
+
+do
+{
+    Console.WriteLine("SELECIONE UMA OPÇÃO ABAIXO:");
+    Console.WriteLine("");
+    Console.WriteLine("1 - CADASTRAR");
+    Console.WriteLine("2 - MOSTRAR");
+    Console.WriteLine("3 - SAIR");
+
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+
+            Console.WriteLine("ADICIONAR NOME");
+            addNome = Console.ReadLine();
+            nome.Add(addNome);
+
+            Console.WriteLine("ADICIONAR CARGO");
+            addCargo = Console.ReadLine();
+            cargo.Add(addCargo);
+
+            Console.WriteLine("ADICIONAR SALARIO");
+            addSalario = float.Parse(Console.ReadLine());
+            salario.Add(addSalario);
+            break;
+
+
+        case 2:
+            Console.WriteLine($"Nome: {addNome} Funcionario: {addCargo} Salário: {addSalario}");
+            break;
+
+
+        case 3:
+            if (opcao == 2)
+            {
+                verdade = false;
+            }
+            break;
+    }
+
+} while (opcao != 3);
