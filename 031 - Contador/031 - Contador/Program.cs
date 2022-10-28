5d94cf9d9a3f2d66db8b24d9f5ff54677a54fdc7
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-28 05:23:14 -0300

    Exercícios de Algoritmos.

---
+++ 031 - Contador/031 - Contador/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int contador = 0;
+string menu;
+
+do
+{
+    Console.WriteLine("carregando " + contador + "%");
+    contador++;
+    //if (contador <= 100) ;
+    //break;
+
+} while (contador <= 100);
