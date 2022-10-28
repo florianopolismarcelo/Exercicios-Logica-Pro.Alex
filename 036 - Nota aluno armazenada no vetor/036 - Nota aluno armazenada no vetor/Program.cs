970d840c970df341c629b5ebbbc87d4c48ded926
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-28 05:41:13 -0300

    Exercícios de Algoritmos.

---
+++ 036 - Nota aluno armazenada no vetor/036 - Nota aluno armazenada no vetor/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média 
+de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.*/
+string[] vetorNomeAluno = new string[3];
+double[] vetorNotaAluno = new double[2];
+double[] vetorMediaAluno = new double[1];
+
+double nota = 0, media = 0;
+string nome;
+int i = 0;
+
+for (i = 0; i < 1; i++)
+{
+    Console.WriteLine("Digite o nome do aluno: ");
+    nome = (Console.ReadLine());
+    vetorNomeAluno[i] = nome;
+}
+for (i = 0; i < 4; i++)
+{
+    Console.WriteLine("Digite a nota: ");
+    nota = Convert.ToInt32(Console.ReadLine());
+    vetorNotaAluno[i] = nota;
+}
+
+//for (int i = 0; i < 4; i++)
+//{
+//    Console.Clear();
+//    Console.WriteLine("Digite a nota: ");
+//    nota = Convert.ToInt32(Console.ReadLine());
+//    vetorNotaAluno[i] = nota;
+//}
+
+//Console.WriteLine("As notas são: ");
+
+//for (int i = 0; i < 4; i++)
+
+//{
+//    Console.WriteLine(vetorNotaAluno[i]);
+//    media = vetorNotaAluno[0] + vetorNotaAluno[1] + vetorNotaAluno[2] + vetorNotaAluno[3];
+//    media = media / 4;
+//}
+//Console.WriteLine($"A Media é: {media}");
