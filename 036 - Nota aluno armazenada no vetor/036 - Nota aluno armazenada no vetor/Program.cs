5b7a466a36426ba2d762bea78a5e2273cc011ae6
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-10-29 08:09:25 -0300

    Exercícios de Algoritmos.

--- 036 - Nota aluno armazenada no vetor/036 - Nota aluno armazenada no vetor/Program.cs
+++ 036 - Nota aluno armazenada no vetor/036 - Nota aluno armazenada no vetor/Program.cs
@@ -3,31 +3,36 @@
 /*Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média 
 de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.*/
 
-string[] vetorNomeAluno = new string[3];
+//string[] vetorNomeAluno = new string[10];
+int[] vetorNomeAluno = new int[2];
 double[] vetorNotaAluno = new double[4];
 double[] vetorMediaAluno = new double[1];
 
 double nota = 0, media = 0;
 string nome;
-int i = 0;
+int i = 0, a=0;
 
-for (i = 0; i < 1; i++)
+for (i = 0; i < vetorNomeAluno.Length; i++)
 {
-    Console.WriteLine("Digite o nome do aluno: ");
-    nome = (Console.ReadLine());
-    vetorNomeAluno[i] = nome;
-}
-for (i = 0; i < 4; i++)
-{
-    Console.WriteLine("Digite a nota: ");
-    nota = Convert.ToInt32(Console.ReadLine());
-    vetorNotaAluno[i] = nota;
-}
-for (i = 0; i < 4; i++)
 
-{
-    Console.WriteLine(vetorNotaAluno[i]);
-    media = vetorNotaAluno[0] + vetorNotaAluno[1] + vetorNotaAluno[2] + vetorNotaAluno[3];
-    media = media / 4;
+
+    for (i = 0; i < 4; i++)
+    {
+        Console.WriteLine("Digite a nota: ");
+        nota = Convert.ToInt32(Console.ReadLine());
+        vetorNotaAluno[i] = nota;
+
+        media = vetorNotaAluno[0] + vetorNotaAluno[1] + vetorNotaAluno[2] + vetorNotaAluno[3];
+        media = media / 4;
+        vetorMediaAluno[i] = media;
+
+
+        //if (media >= 7)
+        //{
+        //    vetorMediaAluno[i] = media;
+        //    Console.WriteLine(vetorMediaAluno[i]);
+        //}
+
+    }
+    Console.WriteLine($"A Media é: {media}");
 }
-Console.WriteLine($"a media é: {media}");
