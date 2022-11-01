b221e54d12c2a01beb4d204e8fbdbbe17d95140b
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-11-01 15:07:26 -0300

    Exercícios de Algoritmos.

--- 036 - Nota aluno armazenada no vetor/036 - Nota aluno armazenada no vetor/Program.cs
+++ 036 - Nota aluno armazenada no vetor/036 - Nota aluno armazenada no vetor/Program.cs
@@ -11,28 +11,46 @@ double nota = 0, media = 0;
 
 int n = 0, i = 0;
 
-Console.WriteLine("---------------");
-Console.WriteLine("Notas do Aluno: ");
-Console.WriteLine("---------------");
 
-for (i = 0; i <=3; i++)
+for (i = 0; i <2; i++)
 {
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("Nota do aluno na posição {0}: ", i);
+    Console.WriteLine("---------------------------");
+
     for (n = 0; n < 4; n++)
     {
-        //Console.Clear();
-        Console.WriteLine("Digite a nota: ");
+        Console.Write("Digite a nota: ");
         nota = Convert.ToInt32(Console.ReadLine());
         vetorNotaAluno[n] = nota;
 
         media = vetorNotaAluno[0] + vetorNotaAluno[1] + vetorNotaAluno[2] + vetorNotaAluno[3];
         media = media / 4;
         vetorMediaAluno[i] = media;
-        media = media + 1;
     }
-    if (media >= 7)
+}
+Console.WriteLine("----------------");
+Console.WriteLine("Todas as médias: ");
+Console.WriteLine("----------------");
+
+
+for (i = 0; i < 2; i++)
     {
-        vetorMediaAluno[i] = media;
-        Console.WriteLine("Nota acima de 7: ");
+        Console.WriteLine(vetorMediaAluno[i]);
+    }
+Console.WriteLine("---------------------------");
+Console.WriteLine("Médias igual ou acima de 7: ");
+Console.WriteLine("---------------------------");
+
+for (i = 0; i < 2; i++)
+{
+    if (vetorMediaAluno[i] >= 7)
+    {
+        //vetorMediaAluno[i] = media;
         Console.WriteLine(vetorMediaAluno[i]);
     }
 }
+
+
+
+
