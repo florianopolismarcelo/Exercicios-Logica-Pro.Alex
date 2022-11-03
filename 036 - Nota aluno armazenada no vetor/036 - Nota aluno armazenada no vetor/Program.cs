40c26dfaa344fd1fd817f2f3472dceba3c477278
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-11-03 13:23:47 -0300

    Exercícios de Algoritmos.

--- 036 - Nota aluno armazenada no vetor/036 - Nota aluno armazenada no vetor/Program.cs
+++ 036 - Nota aluno armazenada no vetor/036 - Nota aluno armazenada no vetor/Program.cs
@@ -51,7 +51,35 @@ for (i = 0; i < 2; i++)
 
 
 
+//*Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.*/
 
+//int[] vetorNumeros = new int[5];
+//int[] vetorSoma = new int[5];
+//int[] vetorMultiplicacao = new int[5];
+
+//int numero = 0, soma = 0, multiplicacao = 0, i = 0;
+
+//for (i = 0; i < 5; i++)
+//{
+//    //Console.WriteLine("Digite o número na posição {0}: ", i);
+//    Console.Clear();
+//    Console.Write("Digite um numero: ");
+//    numero = Convert.ToInt32(Console.ReadLine());
+//    vetorNumeros[i] = numero;
+
+//    soma = vetorNumeros[0] + vetorNumeros[1] + vetorNumeros[2] + vetorNumeros[3] + vetorNumeros[4];
+
+//    multiplicacao = vetorNumeros[1] * vetorNumeros[2] * vetorNumeros[3] * vetorNumeros[4];
+//}
+//Console.WriteLine("Os números digitados são: ");
+//for (i = 0; i < 5; i++)
+//{
+//    Console.WriteLine(vetorNumeros[i]);
+//}
+//Console.Write("A soma de todos os números é: ");
+//Console.WriteLine(soma);
+//Console.Write("A multiplicação de todos os números é: ");
+//Console.WriteLine(multiplicacao);
 
 
 
